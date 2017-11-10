f637632c0e58cc047ee27c62d4b969842caba577
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-11-10 18:30:27 -0500

    Chapter 7, Section 4: Hooking Up the Interpreter

--- src/com/rdodson41/lox/Interpreter.java
+++ src/com/rdodson41/lox/Interpreter.java
@@ -1,6 +1,15 @@
 package com.rdodson41.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
+
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -55,6 +64,21 @@ class Interpreter implements Expr.Visitor<Object> {
       return false;
 
     return a.equals(b);
+  }
+
+  private String stringify(Object object) {
+    if (object == null)
+      return "nil";
+
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
   }
 
   private Object evaluate(Expr expr) {
